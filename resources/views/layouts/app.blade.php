0938a29cde56c56e37d738c68429d6fc4ec0982d
Rustam Aliyev <rustamaliyev@dhcp-10-105-184-48.wireless.northwestern.private>
2018-05-02 14:22:18 -0500

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,7 +98,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">Dashboard</h1>
-                        <?php echo url('/'); ?>
+                       
                         @yield('content')
                         
                     </div>
